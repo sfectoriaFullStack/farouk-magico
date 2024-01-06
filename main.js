3724ecad2a5ff52db7dcf734c4dda9441c4c2143
Mestiri77 <faroukmestiri77@gmail.com>
2024-01-06 21:57:34 +0100

    final in progress 80%

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function flipCard(card) {
+function flipCard(card,img) {
     card.classList.toggle("flipped");
     if (card.childNodes[1].getAttribute("src") === "./assets/couverture.webp") {
-      card.childNodes[1].setAttribute("src", "./assets/dame.png"); 
+      card.childNodes[1].setAttribute("src", img); 
     } else {
       card.childNodes[1].setAttribute("src", "./assets/couverture.webp");
     }
